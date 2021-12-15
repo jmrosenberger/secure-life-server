39c8f9ab00c1bfdcf171a9168aa8546b34e77c4a
jmrosenberger <jmrosenberger0811@yahoo.com>
2021-12-15 14:48:11 -0600

    completed CRUD for Growth component

--- securelifeapi/admin.py
+++ securelifeapi/admin.py
@@ -1,12 +1,12 @@
 from django.contrib import admin
-from securelifeapi.models import Adventure, City, Country, HumanProgress, Human, Image, JournalEntry, Location, Park, PlacesVisited, State, Tag
+from securelifeapi.models import Adventure, City, Country, Growth, Human, Image, JournalEntry, Location, Park, PlacesVisited, State, Tag
 
 # Register your models here.
 
 admin.site.register(Adventure)
 admin.site.register(City)
 admin.site.register(Country)
-admin.site.register(HumanProgress)
+admin.site.register(Growth)
 admin.site.register(Human)
 admin.site.register(Image)
 admin.site.register(JournalEntry)
